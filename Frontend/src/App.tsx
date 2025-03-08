7eb096483abb6821eee996da327fc7975bde6eac
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-03-08 12:56:49 -0800

    Made updates to grid, randomly selected cells turn red on click and display alert

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Grid from './components/grid.tsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +30,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
+
+      <Grid difficulty={"Easy"}/>
     </>
   )
 }
 
 export default App
+
+
+
