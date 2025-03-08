18dbe585ad38eeaef016ace9f4e0adb8288d5cf9
brodrigues2005 <rodriguesbrett85@gmail.com>
2025-03-08 14:57:41 -0800

    Finished music and gird component, now must speed up music when user gets a point, also add point sysytem

---
+++ Frontend/src/components/background-noise.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const audio = new Audio ("/suspense-horror.mp3");
+function Noise({speed}: {speed:number;}) {
+    const [soundPlaying, setPlaySound] = useState(false);
+    
+    const handleBtnClick = () => {
+        setPlaySound(!soundPlaying);
+        console.log(soundPlaying)
+        if (soundPlaying){
+            audio.playbackRate = speed ;
+            audio.loop = true;
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }
+
+
+    // return null;
+    return(<>
+    <button onClick={handleBtnClick}>Toggle Sound</button>
+    </>)
+
+}
+
+export default Noise;
